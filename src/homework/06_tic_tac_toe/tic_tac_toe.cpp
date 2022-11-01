d9fca20a1eef39c6b774db2744194d0f50e02189
ruthiejaej <patricia.morales2@g.austincc.edu>
2022-11-01 18:38:54 +0000

    Homework 06 (cleaned up a little)

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -13,29 +13,23 @@ void tic_tac_toe::set_next_player()
 
 bool tic_tac_toe::check_board_full()
 {
-    bool board_full;
-    if ((pegs.begin(), pegs.end(), " "))
-    {
-        board_full = false;
+    for(int i = 0; i < pegs.size(); i++){
+        if(pegs[i] == " ") {
+            return false;
+        }
     }
-    else
-    {
-        board_full = true;
-    }
-
-    return board_full; 
+    return true;
 }
 
 void tic_tac_toe::clear_board()
 {
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < 9; i++)
 {
-    for (int j = 0; j < 3; j++)
-    {
-    pegs[i][j] = 0;
-    }
-} 
     
+    pegs[i]= " ";
+    
+} 
+   player = "X"; 
 }
 
 bool tic_tac_toe::game_over()
@@ -45,15 +39,18 @@ return check_board_full();
 
 void tic_tac_toe::start_game(string first_player)
 {
-//clear_board();
-if (first_player == "X" || first_player == "O")
-player = first_player;
+    //clear_board();
+    if (first_player == "X" || first_player == "O")
+        player = first_player;
 }
 
 void tic_tac_toe::mark_board(int position)
 {
- pegs[position - 1] = player;
-    set_next_player();
+    if (pegs[position - 1] == " ") {
+        pegs[position - 1] = player;
+    
+        set_next_player();
+    }
 }
 
 string tic_tac_toe::get_player() const
@@ -63,11 +60,10 @@ return player;
 
 void tic_tac_toe::display_board() const
 {
-int size =3;    
-     for (int row = 0; row < size; row++){        
-        for (int column = 0; column < size; column++){       
-            cout <<'\t'<< pegs[row][column];
-    }
-    cout << endl;
-     }
+    cout << pegs[0] << "|" << pegs[1] << "|" << pegs[2] << endl; 
+    cout << pegs[3] << "|" << pegs[4] << "|" << pegs[5] << endl;
+    cout << pegs[6] << "|" << pegs[7] << "|" << pegs[8] << endl;
 }
+    
+     
+
